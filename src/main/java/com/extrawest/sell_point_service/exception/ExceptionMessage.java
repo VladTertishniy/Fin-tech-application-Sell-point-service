30bae90dd726be9e1ec799ef92f4d30edba043de
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2022-10-19 20:20:43 +0300

    added new exception message

--- src/main/java/com/extrawest/sell_point_service/exception/ExceptionMessage.java
+++ src/main/java/com/extrawest/sell_point_service/exception/ExceptionMessage.java
@@ -12,6 +12,7 @@ public enum ExceptionMessage {
     INVALID_TOKEN("JWT token is expired or invalid"),
     NO_USER("There's no user with id: "),
     NO_RIGHTS("You have no rights for doing this!"),
+    WEB_SOCKET_SENDING_ERROR("Error while sending message via WebSocket"),
     FILE_SAVING_ERROR("Error saving file, something went wrong...");
 
     private final String exMessage;
