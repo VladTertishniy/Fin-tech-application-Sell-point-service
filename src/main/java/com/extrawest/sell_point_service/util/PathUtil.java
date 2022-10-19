d3dd5e48a92dc24dd63c69ce481afa9fc1b8dfb4
vladyslav.tertyshnyi <vladyslav.tertyshnyi@extrawest.com>
2022-10-19 20:21:49 +0300

    added new paths PathUtil.java

--- src/main/java/com/extrawest/sell_point_service/util/PathUtil.java
+++ src/main/java/com/extrawest/sell_point_service/util/PathUtil.java
@@ -9,5 +9,7 @@ public class PathUtil {
     public static final String DELETE_PATH = "/delete/{id}";
     public static final String SELL_POINTS_PATH = "/sellPoints";
     public static final String GET_BY_ID_PATH = "/getById/{id}";
+    public static final String GET_ALL_ON_PAGE_PATH = "/getAllOnPage";
     public static final String GET_ALL_PATH = "/getAll";
+    public static final String IS_SELL_POINT_EXIST_PATH = "/isSellPointExist/{id}";
 }
